003756e9f872258907853d234441b5a2be7a9b99
avsharma01 <165581145+avsharma01@users.noreply.github.com>
2025-04-23 18:13:18 +0530

    Update app.py

--- CropSense/app.py
+++ CropSense/app.py
@@ -499,5 +499,6 @@ def predict():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables before running the app
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
     
